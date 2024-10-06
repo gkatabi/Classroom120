310210fa5ee7c961611b3953d011e776b1a48bf9
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-10-06 19:18:14 +0200

    Add files via upload

---
+++ prepare/Learning02/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Provide your names and age!");
+        Console.Write("Enter First Name:");
+        string firstName =           Console.ReadLine();
+        Console.Write("Enter Last Name:");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("State the years you began and ended");
+        Console.Write("Year you began: ");
+        int youBegan = int.perse(Console.ReadLine());
+        Console.Write("yearEnded: ");
+        int yearEnded = int.perse(Console.ReadLine());
+        int Y = ({0} {1}, yearBegan, yearEnded)
+        Console.WriteLine($"{firstName} {lastName}, \n + yearsWorked");
+
+    }
+}
